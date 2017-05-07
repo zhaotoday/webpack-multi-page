6ea88dcd917f5b2f28c8073710daf5f44588ea79
zhaotoday <729234283@qq.com>
2017-05-07 08:08:46 +0800

    update

--- src/app/home/scripts.js
+++ src/app/home/scripts.js
@@ -1,1 +1,10 @@
 import './styles.scss'
+import IScroll from 'iscroll/build/iscroll-lite'
+
+new IScroll('#wrapper', {
+  mouseWheel: true,
+  scrollbars: true,
+  scrollX: true,
+  scrollY: false,
+  freeScroll: true
+})
