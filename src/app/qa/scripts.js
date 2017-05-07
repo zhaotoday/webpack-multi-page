e592e66dc494e123a301400dbf37d13e88052ded
zhaotoday <729234283@qq.com>
2017-05-07 16:13:58 +0800

    update

--- src/app/qa/scripts.js
+++ src/app/qa/scripts.js
@@ -1,19 +1,1 @@
 import './styles.scss'
-
-import IScroll from 'iscroll/build/iscroll-lite'
-
-new IScroll('#wrapper', {
-  mouseWheel: true,
-  scrollbars: true,
-  scrollX: true,
-  scrollY: false,
-  freeScroll: true
-})
-
-$('#protocol').on('tap', function (e) {
-  $(this).toggleClass('pay-confirm__protocol__label--on')
-})
-
-$('#pay').on('tap', function () {
-  alert('pay')
-})
