73197dad013e1a3d02023f39bc732a52696ae05a
820514 <820514@nd>
2017-05-24 19:43:22 +0800

    update

--- src/projects/example/pages/pay/scripts.js
+++ src/projects/example/pages/pay/scripts.js
@@ -1,18 +1,1 @@
 import './styles.scss'
-import IScroll from 'iscroll/build/iscroll-lite'
-import helpers from '../../../../scripts/utils/helpers'
-
-$('#scroller').width(helpers.px2rem($('#scroller li').length * 490 + 30) + 'rem')
-
-new IScroll('#wrapper', {    // eslint-disable-line no-new
-  scrollX: true,
-  scrollY: false
-})
-
-$('#protocol').tap(function (e) {
-  $(this).toggleClass('pay__protocol__label--on')
-})
-
-$('#pay').tap(function () {
-  alert('pay!')
-})
