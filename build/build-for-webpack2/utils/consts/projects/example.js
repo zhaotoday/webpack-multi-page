2093bdc862c30cce909a89d3e46929e6c920c102
zhaotoday <729234283@qq.com>
2017-07-22 20:34:35 +0800

    删除 build-for-webpack2

--- build/build-for-webpack2/utils/consts/projects/example.js
+++
@@ -1,9 +0,0 @@
-const PROJECT = 'example'
-const PAGES = ['one-page']
-const CDN = '/'
-
-module.exports = {
-  PROJECT,
-  PAGES,
-  CDN
-}
--- build/build-for-webpack2/utils/consts/projects/play.js
+++
@@ -1,9 +0,0 @@
-const PROJECT = 'play'
-const PAGES = ['pay', 'guide', 'qa', 'protocol', 'how', 'activity1', 'activity2']
-const CDN = '/'
-
-module.exports = {
-  PROJECT,
-  PAGES,
-  CDN
-}
