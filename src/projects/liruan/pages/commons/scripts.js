036b59d2eab3dc0be59aa64477b140749528fb72
zhaotoday <729234283@qq.com>
2017-08-20 21:28:55 +0800

    优化

--- src/projects/liruan/pages/commons/scripts.js
+++ src/projects/liruan/pages/commons/scripts.js
@@ -1,5 +1,13 @@
-import './styles.scss'
+import '../../styles/global/index.scss';
+
 import './components/panel/styles.scss'
+import './components/breadcrumb/styles.scss'
+import './components/main/styles.scss'
+import './components/sidebar/styles.scss'
+
+import './components/details/styles.scss'
+import './components/list/styles.scss'
+
 import './requires/header/styles.scss'
 import './requires/footer/styles.scss'
 
