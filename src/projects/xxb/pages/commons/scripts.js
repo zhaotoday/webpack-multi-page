b0e0ddf324f835422d898a4e534c3707a4189766
zhaotoday <729234283@qq.com>
2017-08-21 22:26:47 +0800

    update

--- src/projects/xxb/pages/commons/scripts.js
+++ src/projects/xxb/pages/commons/scripts.js
@@ -1,12 +1,8 @@
 import '../../styles/global/index.scss';
 
-import './components/panel/styles.scss'
 import './components/breadcrumb/styles.scss'
 import './components/main/styles.scss'
 import './components/sidebar/styles.scss'
-
-import './components/details/styles.scss'
-import './components/list/styles.scss'
 
 import './requires/header/styles.scss'
 import './requires/footer/styles.scss'
