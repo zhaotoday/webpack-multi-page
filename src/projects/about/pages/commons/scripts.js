9bd2713a567c6b9570407a637203e062f2ce724a
zhaotoday <729234283@qq.com>
2017-08-27 21:31:08 +0800

    update

--- src/projects/about/pages/commons/scripts.js
+++ src/projects/about/pages/commons/scripts.js
@@ -1,11 +1,3 @@
-import '../../styles/global/index.scss';
-
-import './components/breadcrumb/styles.scss'
-import './components/main/styles.scss'
-import './components/sidebar/styles.scss'
-import './components/video-card/styles.scss'
-
+import '../../styles/global/index.scss'
+import './components/downloads/styles.scss'
 import './requires/header/styles.scss'
-import './requires/footer/styles.scss'
-
-document.createElement('article')
--- src/projects/about/pages/home/scripts.js
+++ src/projects/about/pages/home/scripts.js
@@ -1,1 +1,29 @@
 import './styles.scss'
+
+$(function () {
+  var $nav = $('#nav')
+  var $pages = $('#pages')
+
+  $pages.onepage_scroll({
+    sectionContainer: '.page',
+    easing: 'ease',
+    animationTime: 400,
+    pagination: false,
+    updateURL: false,
+    beforeMove: function (index) {
+      $nav.find('li')
+        .eq(index - 1).addClass('active')
+        .siblings().removeClass('active')
+    },
+    afterMove: function (index) {
+    },
+    loop: false,
+    keyboard: true,
+    responsiveFallback: false,
+    direction: 'vertical'
+  })
+
+  $nav.on('click', 'li', function () {
+    $pages.moveTo($(this).data('index'))
+  })
+})
