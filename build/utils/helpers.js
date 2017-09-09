ec2f7e2048e823c0a943bf33a024c5b991db3adc
zhaotoday <729234283@qq.com>
2017-09-10 00:45:19 +0800

    update

--- build/utils/helpers.js
+++ build/utils/helpers.js
@@ -1,12 +1,18 @@
 const consts = require('./consts')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const projectPath = `${consts.SRC}/projects/${consts.PROJECT}/pages`
+const fs = require('fs')
 
 const getEntry = () => {
   const entry = {}
 
   consts.PAGES.forEach(value => {
-    entry[value] = `${projectPath}/${value}/scripts.js`
+    const paths = [
+      `${projectPath}/${value}/scripts.js`,
+      `${projectPath}/${value}/scripts/index.js`
+    ]
+
+    entry[value] = fs.existsSync(paths[0]) ? paths[0] : paths[1]
   })
 
   return entry
