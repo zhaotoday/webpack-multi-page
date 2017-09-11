da05575eb614962480828a10d371fb7a8af31d61
zhaotoday <729234283@qq.com>
2017-09-11 08:00:42 +0800

    update

--- src/projects/xxb/pages/details/scripts.js
+++ src/projects/xxb/pages/details/scripts.js
@@ -3,16 +3,14 @@ import './styles.scss'
 import waterfall from '../../scripts/libs/waterfall'
 
 $(() => {
-    $('#waterfall').waterfall({
-      itemCls: 'waterfall-item',
-      prefix: 'waterfall',
-      fitWidth: true,
-      colWidth: 400,
-      gutterWidth: 20,
-      gutterHeight: 20,
-      loadingMsg: '',
-      debug: false
-    })
-
-    $('.waterfall-item').show()
+  $('#waterfall').waterfall({
+    itemCls: 'waterfall-item',
+    prefix: 'waterfall',
+    fitWidth: true,
+    colWidth: 400,
+    gutterWidth: 20,
+    gutterHeight: 20,
+    loadingMsg: '',
+    debug: false
+  })
 })
