6001674d6e3f844d875c98919cfe62ef6867835c
zhaotoday <729234283@qq.com>
2017-09-14 23:19:25 +0800

    update

--- src/projects/xxb/pages/index/scripts.js
+++ src/projects/xxb/pages/index/scripts.js
@@ -1,1 +1,10 @@
 import './styles.scss'
+
+$(() => {
+  $('#helps-tabs').on('click', 'li', function () {
+    const index = $(this).index()
+
+    $(this).addClass('active').siblings().removeClass('active')
+    $('#helps-content').find('li').eq(index).addClass('active').siblings().removeClass('active')
+  })
+})
