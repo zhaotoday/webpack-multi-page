f27af3adb24640d30c0fc8a15faaccffc629e6c7
820514 <820514@nd>
2018-02-11 21:26:33 +0800

    优化

--- build/handlebars/helpers/getScripts.js
+++ build/handlebars/helpers/getScripts.js
@@ -1,6 +1,6 @@
 const consts = require('../../utils/consts')
 
-module.exports = (project, scripts) => {
+module.exports = scripts => {
   if (typeof scripts === 'string') scripts = [scripts]
 
   return scripts.map(value => `<script src="${consts.CDN}static/scripts/${value}.js"></script>`).join('\n')
--- build/handlebars/helpers/getStyles.js
+++ build/handlebars/helpers/getStyles.js
@@ -1,6 +1,6 @@
 const consts = require('../../utils/consts')
 
-module.exports = (project, styles) => {
+module.exports = styles => {
   if (typeof styles === 'string') styles = [styles]
 
   return styles.map(value => `<link href="${consts.CDN}static/styles/${value}.css" rel="stylesheet">`).join('\n')
