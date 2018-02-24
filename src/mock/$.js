b72b909b254e5eb3593c23a8c874e7901d6b16df
820514 <820514@nd>
2018-02-24 21:35:00 +0800

    优化

--- src/mock/$.js
+++ src/mock/$.js
@@ -4,6 +4,7 @@ module.exports = page => {
 
   return Object.assign({
     styles: [page],
-    scripts: [page]
+    scripts: [page],
+    cdn: process.env.NODE_ENV === 'development' ? '/' : '//lrcdn.cn/project/'
   }, globalData, pageData)
 }
