5a737d7e513bd86bdcb03207c51aa301cdedccb7
zhaojintian <729234283@qq.com>
2018-07-26 17:31:31 +0800

    优化

--- src/mock/$.js
+++ src/mock/$.js
@@ -4,6 +4,11 @@ module.exports = page => {
   const isDev = process.env.NODE_ENV === 'development'
 
   return Object.assign({
+    helpers: {
+      formatTime () {
+        return 2222
+      }
+    },
     consts: {
       CDN: isDev ? `http://localhost:8083` : 'https://cdn.liruan.cn',
       FILE_URL: isDev ? 'http://localhost:3002/apis/v1/files' : ''
