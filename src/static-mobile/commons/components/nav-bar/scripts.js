18036d1861f37b51cb94190425e06fa6ab9b9299
zhaojintian <729234283@qq.com>
2018-07-31 10:08:59 +0800

    优化

--- src/static-mobile/commons/components/nav-bar/scripts.js
+++ src/static-mobile/commons/components/nav-bar/scripts.js
@@ -3,9 +3,9 @@ $(() => {
 
   $navBar.find('li').each(function () {
     const $this = $(this)
-    const views = $this.data('views').split(',')
+    const views = $this.data('route')
 
-    if (views.indexOf(window.VIEW) !== -1) {
+    if (views.indexOf(window.ROUTE) !== -1) {
       $this.addClass('is-active')
     }
   })
